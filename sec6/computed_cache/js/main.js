313719852b0d5c1c95cec7815ac6d05f4d45f526
tommy <tmtmtoa@gmail.com>
2022-05-15 15:03:00 +0900

    computed_cache

---
+++ sec6/computed_cache/js/main.js
@@ -0,0 +1,19 @@
+
+const app = Vue.createApp({
+  data: () => ({
+
+  }),
+  computed: {
+    computedNumber: function () {
+      console.log('computed!')
+      return Math.random()
+    }
+  },
+  methods: {
+    methodsNumber: function () {
+      console.log('methods!')
+      return Math.random()
+    }
+  }
+})
+app.mount('#app')
