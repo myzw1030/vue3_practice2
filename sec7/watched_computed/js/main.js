93bf105b1067279ef4effc8ab826bd20521f2a1c
tommy <tmtmtoa@gmail.com>
2022-05-15 16:06:19 +0900

    watched_computed

---
+++ sec7/watched_computed/js/main.js
@@ -0,0 +1,28 @@
+
+// const app = Vue.createApp({
+//   data: () => ({
+//     firstName: '',
+//     lastName: '',
+//     fullName: ''
+//   }),
+//   watch: {
+//     firstName: function (value) {
+//       this.fullName = value + ' ' + this.lastName
+//     },
+//     lastName: function (value) {
+//       this.fullName = this.firstName + ' ' + value
+//     }
+//   }
+// })
+const app = Vue.createApp({
+  data: () => ({
+    firstName: '',
+    lastName: '',
+  }),
+  computed: {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+})
+app.mount('#app')
